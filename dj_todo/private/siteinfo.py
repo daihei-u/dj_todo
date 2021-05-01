f43b616285e007510cbd3197c20611162f45b6d0
dai <daihei.u@gmail.com>
2021-05-01 20:30:12 +0100

    20210501

---
+++ dj_todo/private/siteinfo.py
@@ -0,0 +1,7 @@
+ANYMAIL = {
+    "MAILGUN_API_KEY": "62091426c4922abca1583da36af6d042-07bc7b05-452a24e5",
+    "MAILGUN_SENDER_DOMAIN": 'sandbox21eb569d5ed2477aad272114c847eee3.mailgun.org',
+}
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+DEFAULT_FROM_EMAIL = "daihei.uk@gmail.com"
+SERVER_EMAIL = "daihei.uk@gmail.com"
